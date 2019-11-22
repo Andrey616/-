05b84ac3a153ce3d58691d3299ebb172bca54ac9
Olegator180s <tvoyapochta@mail.ru>
2019-11-22 17:58:03 +0300

    Added new project

---
+++ Tir/Assets/mishen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class mishen : MonoBehaviour
+{
+
+	float timer;
+	bool side;
+    // Start is called before the first frame update
+    void Start()
+    {
+		side = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		timer += Time.deltaTime;
+if (timer > 2)
+			if(side == true)
+			{
+				side = false;
+				timer = 0;
+			}
+			else
+			{
+				side = true;
+				timer = 0;
+			}
+
+		Move(side);
+	}
+
+	public void Move(bool side)
+	{
+		if(side == true)
+			gameObject.transform.position += new Vector3(Time.deltaTime, 0, 0);
+		else
+			gameObject.transform.position += new Vector3(-Time.deltaTime, 0, 0);
+	}
+}
