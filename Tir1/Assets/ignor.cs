a3dd100cb1db03057ec60de9a5e8f1f21ba9567f
Andrey <andrey.cheban576@gmail.com>
2019-11-25 18:33:36 +0300

    now gun 2

---
+++ Tir1/Assets/ignor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ignor : MonoBehaviour
+{
+	public List<Collider> cols = new List<Collider>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Collider collision in cols)
+		{
+			Physics.IgnoreCollision(collision, GetComponent<Collider>());
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Tir1/Assets/ogom2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ogom2 : MonoBehaviour
+{
+	public GameObject bulletPref;
+	public Transform Pistol_part;
+	
+	public float power = 1f;
+	private GameObject bullet;
+	private void Update()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Shot();
+		}
+		if (gameObject.GetComponent<OVRGrabbable>().isGrabbed)
+		{
+			if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+			{
+				Shot();
+			}
+		}
+	}
+	private void Shot()
+	{
+		bullet = Instantiate(bulletPref, Pistol_part.position, Pistol_part.rotation);
+		
+		bullet.GetComponent<Rigidbody>().AddForce(gameObject.GetComponent<Transform>().forward * power, ForceMode.Impulse);
+		Destroy(bullet, 2f);
+	}
+}
