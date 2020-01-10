83ea7b14b7ef42e044e6f379d6d015bb4ec82b15
Andrey <andrey.cheban576@gmail.com>
2020-01-10 05:44:22 +0300

    Audio good

---
+++ Tir1/Assets/audio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio : MonoBehaviour
+{
+	[SerializeField] AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+	private void Update()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			audioSource.Play();
+		}
+		if (gameObject.GetComponent<OVRGrabbable>().isGrabbed)
+		{
+			if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+			{
+				audioSource.Play();
+			}
+		}
+	}
+	private void OnCollisionEnter(Collision collision)
+	{
+		audioSource.Play();
+	}
+}
